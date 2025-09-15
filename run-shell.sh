338c46b7658b0eb4079a0a6a90f83087a962f704
Jon Davies <jon@hedgerows.org.uk>
2025-09-15 17:03:31 +0000

    update devcontainer environment to use port 8000

--- run-shell.sh
+++ run-shell.sh
@@ -3,4 +3,4 @@ if [ "$1" == "ghcr" ]; then
     image=ghcr.io/jon-hedgerows/docker-lighttpd:dev
 fi
 
-docker run -d --name lighttpd -p 80:80 -v ./test-data/html:/data/html -v ./test-data/conf.d:/data/lighttpd.d --entrypoint=/bin/bash -i $image
+docker run -d --name lighttpd -p 8000:8000 -v ./test-data/html:/data/html -v ./test-data/conf.d:/data/lighttpd.d --entrypoint=/bin/bash -i $image
--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ ! -f test-data/logs/access.log ] ; then
     touch test-data/logs/access.log
     chmod a+rw test-data/logs/access.log
 fi
-docker run -d --name lighttpd -p 80:80 -v ./test-data/html:/data/html -v ./test-data/conf.d:/data/lighttpd.d -v ./test-data/logs:/data/logs $image
+docker run -d --name lighttpd -p 8000:8000 -v ./test-data/html:/data/html -v ./test-data/conf.d:/data/lighttpd.d -v ./test-data/logs:/data/logs $image
